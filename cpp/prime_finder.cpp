aca78e1ffc75b23d9fd8e4d931fb69b0ec98485e
drew <drewruss3@gmail.com>
2023-07-09 16:49:24 -0700

    added C++ source and executable

---
+++ cpp/prime_finder.cpp
@@ -0,0 +1,114 @@
+// g++ -o prime_finder prime_finder.cpp -lgmp
+
+#include <iostream>
+#include <math.h>
+#include <cstdlib>
+#include <cmath>
+#include "prime_functions.h"
+
+using namespace std;
+
+
+int mode = 1;
+
+FILE* primefile;
+
+int main(void) {
+
+	if (mode == 1) {
+
+		// unsigned int lower = 4*pow(10, 4);
+		// unsigned int upper = 5*pow(10, 4);
+
+		unsigned int lower = 1257787 - 5; // 30402457 - 5;
+		unsigned int upper = 1257787 + 5; // 30402457 + 5;
+
+		unsigned int power = lower;
+
+		unsigned int ntrials = upper-lower;
+		unsigned int counter = 0;
+
+		while (counter < ntrials){
+			printf("\n\n\n`````````````````````````````````````````````````````\n");
+			printf("finding new candidate...\n");
+			while (power < upper){
+				power += 1;
+				if (isPrime(power)){
+					break;
+				}
+			}
+
+			float ndigits = log10(power) + 1;
+			printf("\npower is %.0f digits (%d)\n", ndigits, power);
+
+			bool val = LucasLehmer(power);
+
+			if (val){
+				printf("\ncandidate is prime\n");
+				mpz_t result, base;
+				mpz_inits(result, base, NULL);
+
+				mpz_set_str(base, "2", 10);
+				mpz_pow_ui(result, base, power);
+				mpz_sub_ui(result, result, 1);
+
+				primefile = fopen("prime.txt", "a");
+				fputs("\n", primefile);
+				fprintf(primefile, "%u", power);
+				fputs(" -- ", primefile);
+				mpz_out_str(primefile, 10, result);
+				fputs("\n", primefile);
+				fclose(primefile);
+
+				mpz_clears(result, base, NULL);
+			}
+			else{
+				printf("\ncandidate is not prime\n");
+			}
+
+			if (power > upper){
+				break;
+			}
+
+			counter++;
+		}
+	}
+
+	/*
+	else if (mode == 2){
+		mpz_t lower, upper, base;
+		mpz_inits(lower, upper, base, NULL);
+
+		mpz_set_ui(base, 10);
+		mpz_pow_ui(lower, base, 25000);
+		mpz_pow_ui(upper, base, 35000);
+
+		while (true){
+			printf("\n\n\n`````````````````````````````````````````````````````\n");
+			printf("checking new candidate...\n");
+
+			size_t length = mpz_sizeinbase(lower, 10);
+
+			printf("candidate is %lu digits\n", length);
+
+			if (isPrime_mpz(lower)){
+				printf("\ncandidate was prime\n");
+				primefile = fopen("prime.txt", "a");
+				fputs("\n", primefile);
+				mpz_out_str(primefile, 10, lower);
+				fputs("\n", primefile);
+				fclose(primefile);
+			}
+
+			printf("\ncandidate was not prime\n");
+
+			mpz_add_ui(lower, lower, 1);
+			int check = mpz_cmp(lower, upper);
+			
+			if (check == 0){ break; }
+		}
+	}
+	*/
+
+	return 0;
+}
---
+++ cpp/prime_functions.h
@@ -0,0 +1,237 @@
+#include <gmp.h>
+#include <gmpxx.h>
+
+/****************************************************************************************************************************/
+
+inline void mpz_rshift(mpz_t rop, mpz_t op1, mp_bitcnt_t op2) {
+	mpz_fdiv_q_2exp(rop, op1, op2);
+}
+
+
+
+/****************************************************************************************************************************/
+
+
+void print_gmp(mpz_t val){
+	// printf("val: ");
+	mpz_out_str(stdout, 10, val);
+	printf("\n");
+}
+
+
+/****************************************************************************************************************************/
+
+
+
+bool LucasLehmer(unsigned int c){
+	mpz_t candidate, base, s, zero;
+	mpz_inits(candidate, base, s, zero, NULL);
+
+	mpz_set_ui(candidate, 0);
+	mpz_set_str(base, "2", 10);
+
+	// printf("base (should be 2): ");
+	// print_gmp(base);
+
+	mpz_pow_ui(candidate, base, c);
+	mpz_sub_ui(candidate, candidate, 1);
+
+	// printf("candidate (should be big number): ");
+	// print_gmp(candidate);
+
+	mpz_set_ui(s, 4);
+
+	// printf("initial s (should be 4): ");
+	// print_gmp(s);
+    printf("\n");
+	for(int i = 1; i < c-1; i++){
+		if (i%15==0){
+			float percent = 100*i/(c-1);
+			printf("\rvalidating primality...\t%.0f%% complete (%d/%d)", percent, i, (c-1));
+            
+		}
+        // printf("checking... %d\n", i%15);
+        
+		mpz_mul(s, s, s);
+		mpz_sub_ui(s, s, 2);
+
+		// printf("s^2 - 2 = ");
+		// print_gmp(s);
+
+		mpz_mod(s, s, candidate);
+
+		// printf("s mod x = ");
+		// print_gmp(s);
+	}
+	printf("\n");
+	mpz_set_ui(zero, 0);
+
+	// printf("zero: ");
+	// print_gmp(zero);
+
+	// printf("maybe zero? ");
+	// print_gmp(s);
+
+	int check = mpz_cmp(s, zero);
+
+	if (check == 0){
+		mpz_clears(candidate, base, s, zero, NULL);
+		return true;
+	}
+
+	mpz_clears(candidate, base, s, zero, NULL);
+	return false;
+}
+
+
+
+/****************************************************************************************************************************/
+
+
+
+
+bool trial_composite(mpz_t a, mpz_t d, mpz_t n, mpz_t s){
+	mpz_t t1, t2, t3, nprime, one, two, iterator;
+	mpz_inits(t1, t2, t3, nprime, one, two, iterator, NULL);
+
+	mpz_set_ui(one, 1);
+	mpz_set_ui(two, 2);
+	mpz_sub_ui(nprime, n, 1);
+
+	mpz_powm(t1, a, d, n);
+
+	if (mpz_cmp(t1, one) == 0){
+		mpz_clears(t1, t2, t3, nprime, one, two, iterator, NULL);
+		return false;
+	}
+
+	mpz_set_ui(iterator, 0);
+	while (true){
+		mpz_powm(t2, two, iterator, n);
+		mpz_mul(t2, t2, d);
+
+		mpz_powm(t3, a, t2, n);
+		if (mpz_cmp(t3, nprime) == 0){
+			mpz_clears(t1, t2, t3, nprime,  one, two, iterator, NULL);
+			return false;
+		}
+
+		mpz_add_ui(iterator, iterator, 1);
+                if (mpz_cmp(iterator, s) == 0) { break; }
+	}
+
+	mpz_clears(t1, t2, t3, nprime, one, two, iterator, NULL);
+	return true;
+}
+
+
+
+/****************************************************************************************************************************/
+
+
+bool isPrime(unsigned int value_){
+	unsigned int l = value_%10;
+
+	if (l == 2 || l == 4 || l == 5 || l == 6 || l == 8 || l == 0){
+		return false;
+	}
+
+        mpz_t value, a, d, dprime, s, zero;
+        mpz_inits(value, a, d, dprime, s, zero, NULL);
+
+	mpz_set_ui(value, value_);
+	mpz_set_ui(s, 0);
+	mpz_set_ui(d, value_-1);
+	mpz_set_ui(zero, 0);
+
+	while (true){
+		mpz_mod_ui(dprime, d, 2);
+		if (mpz_cmp(dprime, zero) != 0){
+			break;
+		}
+
+		mpz_rshift(d, d, 1);
+		mpz_add_ui(s, s, 1);
+	}
+
+	gmp_randstate_t rstate;
+    	gmp_randinit_mt(rstate);
+
+	unsigned long seed;
+    	gmp_randseed_ui(rstate, seed);
+
+	for (int i = 0; i < 8; i++){
+		mpz_urandomm(a, rstate, value);
+		if (trial_composite(a, d, value, s)){
+			gmp_randclear(rstate);
+			mpz_clears(value, a, d, dprime, s, zero, NULL);
+			return false;
+		}
+	}
+
+	gmp_randclear(rstate);
+	mpz_clears(value, a, d, dprime, s, zero, NULL);
+	return true;
+}
+
+
+/*
+bool isPrime_mpz(mpz_t value){
+	printf("entered test\n");
+	mpz_t l, a, d, dprime, s, zero;
+	printf("defined items\n");
+	mpz_inits(l, a, d, dprime, s, zero, NULL);
+	printf("initialized to zero\n");
+
+	printf("\n");
+	printf("computing mod...");
+
+	mpz_mod_ui(l, value, 10);
+
+	printf("validating primality...\t%.0f%% complete", 0.0);
+
+	if (mpz_cmp_ui(l, 2) == 0 || mpz_cmp_ui(l, 4) == 0 || mpz_cmp_ui(l, 5) == 0 || mpz_cmp_ui(l, 6) == 0 || mpz_cmp_ui(l, 8) == 0 || mpz_cmp_ui(l, 0) == 0){
+		printf("\rvalidating primality...\t%.0f%% complete", 100.0);
+		printf("\n");
+		return false;
+	}
+
+	mpz_set_ui(s, 0);
+	mpz_sub_ui(d, value, 1);
+	mpz_set_ui(zero, 0);
+
+	while (true){
+		mpz_mod_ui(dprime, d, 2);
+		if (mpz_cmp(dprime, zero) != 0){
+			break;
+		}
+
+		mpz_rshift(d, d, 1);
+		mpz_add_ui(s, s, 1);
+	}
+
+	gmp_randstate_t rstate;
+    	gmp_randinit_mt(rstate);
+
+	unsigned long seed;
+    	gmp_randseed_ui(rstate, seed);
+
+	for (int i = 0; i < 8; i++){
+		float percent = 100*i/8;
+		printf("validating primality...\t%.0f%% complete", percent);
+
+		mpz_urandomm(a, rstate, value);
+		if (trial_composite(a, d, value, s)){
+			gmp_randclear(rstate);
+			mpz_clears(l, a, d, dprime, s, zero, NULL);
+			printf("\n");
+			return false;
+		}
+	}
+
+	gmp_randclear(rstate);
+	mpz_clears(l, a, d, dprime, s, zero, NULL);
+	printf("\n");
+	return true;
+}
+*/
