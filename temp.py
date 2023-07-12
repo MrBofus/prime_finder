d815a76a96df7e9a2279433448a54d2ccb01819f
MrBofus <drewruss3@gmail.com>
2023-07-11 21:01:53 -0700

    modifying python side

--- temp.py
+++ temp.py
@@ -58,9 +58,9 @@ print('time using Miller-Rabin: ' + str(round(t_to_end - t_to_start, 2)) + 's\n\
 '''
 
 '''
-s = 0.0011
+s = 1/12000
 P = 1/s
-print(P*43 / (3600))
+print(P*12 / (3600))
 '''
 
 # p = int(10**10000 - 1)
@@ -70,6 +70,47 @@ with open('resources/mersenne_primes.txt', 'a') as f:
     f.write(str(p) + ' is prime\n\n')
 '''
 
-
+'''
 result = (13**19) % 60
 print(result)
+'''
+
+with open('cpp/quadruplet_search.txt') as f:
+    lines = f.readlines()
+
+# split strings into list of primes
+primelist = []
+for i in range(0, len(lines), 2): 
+    primelist.append(int(lines[i]))
+
+# iterate through each prime stored in the list of primes
+pcounter = 0
+for i in range(1, len(primelist)):
+    # if there is a twin prime, append it to the twin primes text file
+    if primelist[i] - primelist[i-1] == 2:
+        
+        # print('twin prime found')
+        with open('resources/q_twin_primes.txt', 'a') as f:
+            f.write('```````````````````\ntwin primes:\n\n')
+            f.write(str(primelist[i-1]) + '\n\n')
+            f.write(str(primelist[i]) + '\n\n')
+            f.write('```````````````````\n\n\n\n')
+    
+    
+    
+    if pcounter > 0:
+        # if there is a triplet prime, append it to the twin primes text file
+        if ((primelist[i] - primelist[i-1] == 4 and 
+                primelist[i] - primelist[i-2] == 6) or 
+                (primelist[i] - primelist[i-1] == 2 and 
+                    primelist[i] - primelist[i-2] == 6)):
+            
+            # print('triplet prime found')
+            with open('resources/q_twin_primes.txt', 'a') as f:
+                f.write('```````````````````\ntriplet primes:\n\n')
+                f.write(str(primelist[i-2]) + '\n\n')
+                f.write(str(primelist[i-1]) + '\n\n')
+                f.write(str(primelist[i]) + '\n\n')
+                f.write('```````````````````\n\n\n\n')
+    
+    pcounter += 1
