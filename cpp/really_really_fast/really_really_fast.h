d118a4f65a8ee24c939c601c9a4782595207e04c
drew <drewruss3@gmail.com>
2025-03-15 18:45:23 -0500

    making sure we're all synched !! :)

--- cpp/really_really_fast/really_really_fast.h
+++ cpp/really_really_fast/really_really_fast.h
@@ -10,6 +10,7 @@
 #include <ctime>
 #include <tuple>
 #include <chrono>
+#include <omp.h>
 
 using namespace std::chrono;
 using namespace std;
@@ -71,6 +72,30 @@ void print_gmp(mpz_t val){
 	// printf("val: ");
 	mpz_out_str(stdout, 10, val);
 	// printf("\n");
+}
+
+
+/****************************************************************************************************************************/
+
+
+void fast_mpz_powm(mpz_t* res, mpz_t base, mpz_t pow, mpz_t mod){
+	mpz_t last, one;
+	mpz_inits(last, one, NULL);
+
+	while ( mpz_cmp_ui(pow, 0) > 0 ){
+		mpz_and(last, *res, one);
+
+		if ( mpz_cmp_ui(last, 1) == 0){
+			mpz_mul(*res, *res, base);
+		}
+
+		mpz_mul(base, base, base);
+		mpz_rshift(pow, pow, 1);
+
+		mpz_mod(*res, *res, mod);
+	}
+
+	mpz_clears(last, one);
 }
 
 
@@ -216,7 +241,7 @@ bool isPrime_mpz_fast(mpz_t value, gmp_randstate_t rstate, bool verbose){
 		cout.flush();
 	}
 
-	for (int i = 0; i < 12; i++){
+	for (int i = 0; i < 8; i++){
         mpz_urandomm(a, rstate, value);
         if (mpz_cmp_ui(a, 0) == 0) { mpz_set_ui(a, 1); }
         mpz_powm(t1, a, d, value);
@@ -250,6 +275,133 @@ bool isPrime_mpz_fast(mpz_t value, gmp_randstate_t rstate, bool verbose){
 			}
 		}
 	}
+
+	mpz_clears(a, d, dprime, s, NULL);
+   	mpz_clears(t1, t2, t3, nprime, two, NULL);
+
+	return true;
+}
+
+
+
+/****************************************************************************************************************************/
+
+
+bool __split_parrallel__(mpz_t a, mpz_t d, mpz_t dprime, mpz_t s, 
+						 mpz_t t1, mpz_t t2, mpz_t t3, mpz_t nprime, mpz_t two,
+						 mpz_t value, gmp_randstate_t rstate, bool verbose){
+	
+
+	int truth = 0;
+
+	# pragma omp parallel for reduction(+ : truth)
+	for (int i = 0; i < 8; i++){
+
+        mpz_urandomm(a, rstate, value);
+		if (mpz_cmp_ui(a, 0) == 0) { mpz_set_ui(a, 1); }
+		mpz_powm(t1, a, d, value);
+
+		if (verbose){
+			cout << "\n\t-----> starting trial-composite fast test..." << endl;
+			cout.flush();
+		}
+
+		auto start = high_resolution_clock::now();
+		if (trial_composite_fast(t1, t2, t3,
+								nprime, two,
+								a, d, value, s, verbose)){
+
+			if (verbose){
+				auto stop = high_resolution_clock::now();
+				auto duration = duration_cast<microseconds>(stop - start);
+				cout << "\t-----> finished, took " << duration.count() << "us" << endl;
+				cout.flush();
+			}
+
+			mpz_clears(a, d, dprime, s, NULL);
+			mpz_clears(t1, t2, t3, nprime, two, NULL);
+			truth++;
+		}
+		else{
+			if (verbose){
+				auto stop = high_resolution_clock::now();
+				auto duration = duration_cast<microseconds>(stop - start);
+				cout << "\t-----> finished, took " << duration.count() << "us" << endl;
+				cout.flush();
+			}
+		}
+	}
+	
+	if (truth == 0){ return true; } else { return false;}
+}
+
+
+/****************************************************************************************************************************/
+
+
+bool isP_mpz_fast_split_parallel(mpz_t value, gmp_randstate_t rstate, bool verbose){
+
+	mpz_t a, d, dprime, s;
+	mpz_inits(a, d, dprime, s, NULL);
+
+    mpz_t t1, t2, t3, nprime, two;
+    mpz_inits(t1, t2, t3, nprime, two, NULL);
+
+    mpz_set_ui(two, 2);
+    mpz_sub_ui(nprime, value, 1);
+
+	if (verbose){
+		cout << "\n\t-----> checking last digit..." << endl;
+		cout.flush();
+	}
+
+    auto start = high_resolution_clock::now();
+	if (checkLastDigit(value)){
+		if (verbose){
+			auto stop = high_resolution_clock::now();
+			auto duration = duration_cast<microseconds>(stop - start);
+			cout << "\t-----> last digit checked, took " << duration.count() << "us" << endl;
+			cout.flush();
+		}
+
+        mpz_clears(a, d, dprime, s, NULL);
+        mpz_clears(t1, t2, t3, nprime, two, NULL);
+		return false;
+	} else {
+		if (verbose){
+			auto stop = high_resolution_clock::now();
+			auto duration = duration_cast<microseconds>(stop - start);
+			cout << "\t-----> last digit checked, took " << duration.count() << "us" << endl;
+			cout.flush();
+		}}
+
+    mpz_set_ui(s, 0);
+    mpz_sub_ui(d, value, 1);
+
+	if (verbose){
+		cout << "\n\t-----> determining \'s\' and \'d\'..." << endl;
+		cout.flush();
+	}
+
+	start = high_resolution_clock::now();
+    mpz_mod_ui(dprime, d, 2);
+	while (mpz_cmp_ui(dprime, 0) == 0){
+
+		mpz_rshift(d, d, 1);
+		mpz_add_ui(s, s, 1);
+
+        mpz_mod_ui(dprime, d, 2);
+	}
+
+	if (verbose){
+		auto stop = high_resolution_clock::now();
+		auto duration = duration_cast<microseconds>(stop - start);
+		cout << "\t-----> finished, took " << duration.count() << "us" << endl;
+		cout.flush();
+	}
+
+	__split_parrallel__(a, d, dprime, s, t1, t2, t3, nprime, two,
+						value,  rstate, verbose);
 
 	mpz_clears(a, d, dprime, s, NULL);
    	mpz_clears(t1, t2, t3, nprime, two, NULL);
--- cpp/really_really_fast/rrf_pfinder.cpp
+++ cpp/really_really_fast/rrf_pfinder.cpp
@@ -4,93 +4,110 @@
 // import libraries
 #include "really_really_fast.h"
 
-unsigned int n_digits = 50;
-unsigned int n_trials = 100000;
-bool verbose = false;
+unsigned int n_digits = 10;
+unsigned int n_trials = 1000;
+const bool verbose = false;
+
+unsigned int n_trials_macro = 10;
+unsigned int n_trials_micro = 10;
 
 // initialize file to write primes to
 FILE* primefile;
 
-unsigned int counter = 0;
-unsigned int n_primes = 0;
-
 // main loop
 int main(void) {
 
-    // define and initialize random number generator
-    gmp_randstate_t rstate;
-    gmp_randinit_mt(rstate);
+    for ( int l = 0; l < n_trials_macro; l++ ) {
 
-    // define and initialize random seed for rng
-    srand((unsigned int)time(NULL));
-    unsigned long seed = rand()%100000000000;
-    gmp_randseed_ui(rstate, seed);
+        n_digits += 50;
 
-    cout << "generated seed..." << endl;
+        for (int i = 0; i < n_trials_micro; i++){
+
+            unsigned int counter = 0;
+            unsigned int n_primes = 0;
+
+            // define and initialize random number generator
+            gmp_randstate_t rstate;
+            gmp_randinit_mt(rstate);
+
+            // define and initialize random seed for rng
+            srand((unsigned int)time(NULL));
+            unsigned long seed = rand()%100000000000;
+            gmp_randseed_ui(rstate, seed);
+
+            if (verbose){cout << "generated seed..." << endl;}
+
+            mpz_t upper, base, random;
+            mpz_inits(upper, base, random, NULL);
+
+            // intialize 'base' to 10
+            mpz_set_ui(base, 10);
+
+            // define upper bound values given number of digits
+            mpz_pow_ui(upper, base, n_digits);
+            
+            // generate random number in range
+            mpz_urandomm(random, rstate, upper);
 
 
 
-    mpz_t upper, base, random;
-	mpz_inits(upper, base, random, NULL);
+            //start timer
+            auto start = high_resolution_clock::now();
+            if (verbose){cout << "started timer..." << endl;}
 
-    // intialize 'base' to 10
-	mpz_set_ui(base, 10);
+            while (counter < n_trials){
 
-    // define upper bound values given number of digits
-    mpz_pow_ui(upper, base, n_digits);
-    
-    // generate random number in range
-    mpz_urandomm(random, rstate, upper);
+                if (verbose){
+                    cout << "\nchecking candidate" << endl;
+                    cout.flush();
+                }
 
+                if (isPrime_mpz_fast(random, rstate, verbose)) {
+                    if (verbose){
+                        cout << "\ncandidate was prime\n" << endl;
+                        cout.flush(); 
+                    }
+                    /*
+                    primefile = fopen("p_out.txt", "a");
+                    fputs("\n", primefile);
+                    mpz_out_str(primefile, 10, random);
+                    fputs("\n", primefile);
 
+                    fclose(primefile);
+                    */
 
-    //start timer
-    auto start = high_resolution_clock::now();
-    cout << "started timer..." << endl;
+                    n_primes++;
+                } else{
+                    if (verbose){
+                        cout << "\t-----> test failed" << endl;
+                        cout << "\ncandidate was not prime\n" << endl;
+                        cout.flush();
+                }
+                }
 
-    while (counter < n_trials){
-
-        if (verbose){
-            cout << "\nchecking candidate" << endl;
-			cout.flush();
-        }
-
-        if (isPrime_mpz_fast(random, rstate, verbose)) {
-            if (verbose){
-                cout << "\ncandidate was prime\n" << endl;
-                cout.flush(); 
+                // increment candidate by one
+                mpz_add_ui(random, random, 1);
+                counter++;
             }
-            // primefile = fopen("p_out.txt", "a");
-            // fputs("\n", primefile);
-            // mpz_out_str(primefile, 10, random);
-            // fputs("\n", primefile);
 
-            // fclose(primefile);
+            if (verbose){cout << "finished" << endl;}
+            
+            // stop timer
+            auto stop = high_resolution_clock::now();
+            auto duration = duration_cast<microseconds>(stop - start);
 
-            n_primes++;
-        } else{
-            if (verbose){
-                cout << "\t-----> test failed" << endl;
-                cout << "\ncandidate was not prime\n" << endl;
-                cout.flush();
+            if (verbose){cout << "\ncode took " << duration.count()/1000 << "ms to check " << n_trials << " candiates, finding " << n_primes << "\n\t(rate of " << 1000000*n_trials/duration.count() << " trials per second)" << endl;}
+            else {cout << n_digits << ",\t" << duration.count() << endl;}
+
+            primefile = fopen("t_out.txt", "a");
+            fprintf(primefile, "%d,\t%ld\n", n_digits, duration.count());
+            fclose(primefile);
+            
+
+            gmp_randclear(rstate);
+            mpz_clears(upper, base, random, NULL);
         }
-        }
-
-        // increment candidate by one
-		mpz_add_ui(random, random, 1);
-        counter++;
     }
-
-    cout << "finished" << endl;
-
-    //start timer
-    auto stop = high_resolution_clock::now();
-    auto duration = duration_cast<microseconds>(stop - start);
-
-    cout << "\ncode took " << duration.count()/1000 << "ms to check " << n_trials << " candiates, finding " << n_primes << "\n\t(rate of " << 1000000*n_trials/duration.count() << " trials per second)" << endl;
-
-    gmp_randclear(rstate);
-    mpz_clears(upper, base, random, NULL);
+     
     return 0;
-
 }
